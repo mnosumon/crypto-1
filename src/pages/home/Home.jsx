de8efa5527e3ee66ff2b987aa85426950b267e63
Md Nuruddin Osman <mdnuruddin378350@gmail.com>
2024-06-01 23:57:46 +0600

    Product Feature

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,8 +9,8 @@ const Home = () => {
   return (
     <>
         <Bannner/>
-        {/* <ProductFeature/>
-        <About/>
+        <ProductFeature/>
+        {/* <About/>
         <Question/>
         <Clins/> */}
     </>
