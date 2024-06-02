b44360819a77aaedf308117cd5e7b5f62b7a1664
Md Nuruddin Osman <mdnuruddin378350@gmail.com>
2024-06-02 13:47:52 +0600

    About part Responsive Done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -10,8 +10,8 @@ const Home = () => {
     <>
         <Bannner/>
         <ProductFeature/>
-        {/* <About/>
-        <Question/>
+        <About/>
+        {/* <Question/>
         <Clins/> */}
     </>
   )
