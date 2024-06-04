7c58869322f4efd9cd5ea391cb5e3b30679a1e16
Md Nuruddin Osman <mdnuruddin378350@gmail.com>
2024-06-04 12:20:37 +0600

    form edit in css

--- src/pages/singup/SignUp.jsx
+++ src/pages/singup/SignUp.jsx
@@ -5,12 +5,13 @@ import { Link } from 'react-router-dom'
 import { useFormik } from 'formik';
 
 let initialState = {
-    fullName: "",
+    firstName: "",
+    lastName: "",
     email: "",
     password: "",
-    id: "",
-    phone: "",
-    role: "",
+    birthYear: "",
+    birthMonth: "",
+    birthDay: "",
     terms: "",
 }
 
@@ -34,26 +35,47 @@ const SignUp = () => {
                     </div>
                     <form onSubmit={formik.handleSubmit} action="" className='form'>
                         <div className="form_wrapper">
-                            <input onChange={formik.handleChange} onBlur={formik.handleBlur} autoComplete='off' value={formik.values.fullName}  className='registerField' type='text' placeholder='Enter Your Full Name' name='fullName'/>
+                            <input onChange={formik.handleChange} onBlur={formik.handleBlur} autoComplete='off' value={formik.values.firstName}  className='registerField' type='text' placeholder='Enter Your First Name' name='firstName'/>
+
+                            <input onChange={formik.handleChange} onBlur={formik.handleBlur} autoComplete='off' value={formik.values.lastName}  className='registerField' type='text' placeholder='Enter Your last Name' name='lastName'/>
 
                             <input onChange={formik.handleChange} onBlur={formik.handleBlur} autoComplete='off' value={formik.values.email} className='registerField' type='email' placeholder='Enter Your Email' name='email'/>
-
-                            <input onChange={formik.handleChange} onBlur={formik.handleBlur} autoComplete='off' value={formik.values.id} className='registerField' type='number' placeholder='your ID' name='id'/>
-
-                            <input onChange={formik.handleChange} onBlur={formik.handleBlur} autoComplete='off' value={formik.values.phone} className='registerField' type='tel' placeholder='Your Mobile Number' name='phone'/>
                             
                             <input onChange={formik.handleChange} onBlur={formik.handleBlur} autoComplete='off' value={formik.values.password} className='registerField' type="password" placeholder='Password' name='password'/>
 
                             <input onChange={formik.handleChange} onBlur={formik.handleBlur} autoComplete='off' value={formik.values.password} className='registerField' type="password" placeholder='Confirmed Password' name='password'/>
 
-                            <select className='registerField' name="role" id="role">
-                                <option>Select your role</option>
-                                <option>333</option>
-                                <option>444</option>
-                                <option>555</option>
-                            </select>
+                            <div className="birth">
+                                <select className='birthField' name="birthYear" id="role">
+                                    <option>Birth year</option>
+                                    <option>333</option>
+                                    <option>444</option>
+                                    <option>555</option>
+                                </select>
+                                <select className='birthField' name="birthMonth" id="role">
+                                    <option>Birth Month</option>
+                                    <option>333</option>
+                                    <option>444</option>
+                                    <option>555</option>
+                                </select>
+                                <select className='birthField' name="birthDay" id="role">
+                                    <option>Birth day</option>
+                                    <option>333</option>
+                                    <option>444</option>
+                                    <option>555</option>
+                                </select>
+                            </div>
+                            <div className='genderPart'>
+                                <p >Select your gender</p>
+                                <div className="gender">
+                                    <input type="radio" id='male' name='gender'/>
+                                    <label htmlFor="male">Male</label>
+                                    <input type="radio" id='female' name='gender' />
+                                    <label htmlFor="female">Female</label>
+                                </div>
+                            </div>
                             <div className="checkbox">
-                                <input type="checkbox" checked="checked" className='check_Field' />
+                                <input type="checkbox" className='check_Field' />
                                 <p className='checkboxTerm'> I read and agree to the <Link>Terms & Conditions</Link></p>
                             </div>
                         </div>
